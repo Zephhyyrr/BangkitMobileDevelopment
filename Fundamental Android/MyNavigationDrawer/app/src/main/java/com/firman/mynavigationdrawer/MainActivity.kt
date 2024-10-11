40d257070bff270ecf4bb8b9cda4d3c489c68b8d
Firman Ardiansyah <ardiansyahfirman2004@gmail.com>
2024-10-11 19:54:01 +0700

    add MyBottomNavigation

--- Fundamental Android/MyNavigationDrawer/app/src/main/java/com/firman/mynavigationdrawer/MainActivity.kt
+++ Fundamental Android/MyNavigationDrawer/app/src/main/java/com/firman/mynavigationdrawer/MainActivity.kt
@@ -11,12 +11,18 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.firman.mynavigationdrawer.databinding.ActivityMainBinding
+import de.hdodenhof.circleimageview.CircleImageView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var profileCircleImageView: CircleImageView
+    private var profileImageUrl =
+        "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/462064fa-8a78-4289-82e4-375cc0c67d47/dg8xas3-79e7db6d-5154-4a88-89ca-4968b4dfb506.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzQ2MjA2NGZhLThhNzgtNDI4OS04MmU0LTM3NWNjMGM2N2Q0N1wvZGc4eGFzMy03OWU3ZGI2ZC01MTU0LTRhODgtODljYS00OTY4YjRkZmI1MDYuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.V8CNSVSApirnIMnl5BEi5tEFsfSeqMXiFtNA6HEFFts"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,11 +40,15 @@ class MainActivity : AppCompatActivity() {
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
+        profileCircleImageView = navView.getHeaderView(0).findViewById(R.id.imageView)
+        Glide.with(this)
+            .load(profileImageUrl)
+            .into(profileCircleImageView)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_cart
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
