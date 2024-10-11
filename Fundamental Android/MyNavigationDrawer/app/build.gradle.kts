bd65cb6bee2e5e0c8372fc6830f740c63b2e5c1f
Firman Ardiansyah <ardiansyahfirman2004@gmail.com>
2024-10-11 17:56:58 +0700

    add MyFlexibleFragment

--- Fundamental Android/MyNavigationDrawer/app/build.gradle.kts
+++ Fundamental Android/MyNavigationDrawer/app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.firman.myappbar"
+    namespace = "com.firman.mynavigationdrawer"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.firman.myappbar"
+        applicationId = "com.firman.mynavigationdrawer"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -33,8 +33,8 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    viewBinding{
-        enable = true
+    buildFeatures {
+        viewBinding = true
     }
 }
 
@@ -43,8 +43,11 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
