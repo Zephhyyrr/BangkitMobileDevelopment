a5170a80758169b7f9017bd5e7b483f57dbff708
Firman Ardiansyah <ardiansyahfirman2004@gmail.com>
2024-10-13 11:35:31 +0700

    edit RestaurantReview to LiveData

--- Fundamental Android/RestaurantReview/app/build.gradle.kts
+++ Fundamental Android/RestaurantReview/app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.retrofit2.converter.gson)
     implementation(libs.logging.interceptor)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
